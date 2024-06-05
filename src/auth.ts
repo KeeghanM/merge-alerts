4ea787f8adf147b33641cea69333c84175d8e521
Keeghan McGarry <keeghan_m@live.com>
2024-06-05 17:11:57 +0100

    removed random words

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,6 @@ import NextAuth from 'next-auth'
 import Resend from 'next-auth/providers/resend'
 import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import { db } from '@/db/db'
-import { generate } from 'random-words'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -10,15 +9,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Resend({
       apiKey: process.env.AUTH_RESEND_KEY,
       from: 'auth@keeghan.io',
-      async generateVerificationToken() {
-        return generate({
-          exactly: 1,
-          wordsPerString: 3,
-          separator: '-',
-          minLength: 4,
-          maxLength: 7,
-        }) as string
-      },
     }),
   ],
 })
